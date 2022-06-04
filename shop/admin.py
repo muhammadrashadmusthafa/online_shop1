e33acdb3ccb8a51afff6832c12756390079e6475
ichu <muhammadrashadmusthafa@gmail.com>
2022-06-05 04:37:30 +0530

    cart for adding products

--- shop/admin.py
+++ shop/admin.py
@@ -14,3 +14,11 @@ class ProductAdmin( admin.ModelAdmin):
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
+
+
+from django.contrib.sessions.models import Session
+class SessionAdmin(admin.ModelAdmin):
+    def _session_data(self, obj):
+        return obj.get_decoded()
+    list_display = ['session_key', '_session_data', 'expire_date']
+admin.site.register(Session, SessionAdmin)
