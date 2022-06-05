5dfa260485df76d462e248a9c665e205c42bd5dd
ichu <muhammadrashadmusthafa@gmail.com>
2022-06-05 14:38:58 +0530

    registering customer orders

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -5,8 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/', include('shop.urls', namespace='shop')),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
+    path('', include('shop.urls', namespace='shop')),
 ]
 
 if settings.DEBUG:
