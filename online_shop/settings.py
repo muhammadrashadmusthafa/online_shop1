51042f0cbbcd6fee59540933e9aae4749d4cf1d9
ichu <muhammadrashadmusthafa@gmail.com>
2022-06-07 17:55:41 +0530

    launching asynchronous task with celery

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -127,3 +127,17 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
+
+
+# E-mail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
+# For RabbitMQ
+CELERY_BROKER_URL = 'amqp://localhost'
